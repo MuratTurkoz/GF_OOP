9c79b2422c6af9c50ae622a69e9e1ee59db62ca1
MuratTurkoz <44347576+MuratTurkoz@users.noreply.github.com>
2023-07-07 23:05:53 +0300

    added lesson two

---
+++ GFA/GFA.OOP/Models/GameAccount.cs
@@ -0,0 +1,39 @@
+﻿using GFA.OOP.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFA.OOP.Models
+{
+    internal class GameAccount
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Platform Platform { get; set; }
+        public int Level { get; set; }
+        public string GameName { get; set; }
+        public decimal Price { get; set; }
+        public static Platform ConvertStringToPlatform(string platform)
+        {
+            switch (platform.ToLower())
+            {
+                case "steam":
+                    return Platform.Steam;
+                case "origin":
+                    return Platform.Origin;
+                case "battle.net":
+                    return Platform.BattleNet;
+                case "uplay":
+                    return Platform.Uplay;
+                case "epic games":
+                    return Platform.EpicGames;
+
+                default:
+                    throw new Exception("Unex");
+            }
+        }
+    }
+}
--- GFA/GFA.OOP/Program.cs
+++ GFA/GFA.OOP/Program.cs
@@ -1,33 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using GFA.OOP.Enums;
+
 using GFA.OOP.Models;
 
-var firstPlayer = new Player("Gandhalf 38", 38, Race.Human,PlayerClassType.Mage);
+var fileText = File.ReadAllText("C:\\Users\\Murat\\Desktop\\RussianMob.txt");
+List<GameAccount> accounts = new List<GameAccount>();
+foreach (string file in fileText.Split("\n"))
+{
+    List<string> files = file.Split("---").ToList();
+    GameAccount account = new GameAccount()
+    {
+        Id = 1,
+        UserName = files[0],
+        Password = files[1],
+        Platform = GameAccount.ConvertStringToPlatform(files[2]),
+        Level = Convert.ToInt32(files[3]),
+        GameName = files[4],
+        Price = Convert.ToDecimal(files[5].Replace("$", string.Empty)),
+    };
 
-Console.WriteLine($"ID:{firstPlayer.Id}, Character Name:{firstPlayer.Name}, Character Level:{firstPlayer.Level}, Character Favourite Number:{firstPlayer.FavouriteNumber}");
+    accounts.Add(account);
 
-firstPlayer.LevelUp();
-firstPlayer.LevelUp();
-firstPlayer.LevelUp();
-firstPlayer.LevelUp();
-firstPlayer.LevelUp();
+    //Console.WriteLine(file);
+    //Console.ReadLine();
 
-Console.WriteLine($"ID:{firstPlayer.Id}, Character Name:{firstPlayer.Name}, Character Level:{firstPlayer.Level}, Character Favourite Number:{firstPlayer.FavouriteNumber}");
-
-firstPlayer.ChangeRace(Race.Dwarf);
-
-Console.WriteLine(firstPlayer.Race.ToString());
-
-var secondPlayer = new Player("Excalibur",10,Race.Orc,PlayerClassType.DeathKnight);
-
-Console.WriteLine($"ID:{secondPlayer.Id}, Character Name:{secondPlayer.Name}, Character Level:{secondPlayer.Level}, Character Favourite Number:{secondPlayer.FavouriteNumber}");
-
-var thirdPlayer = new Player("Tarkan", 55, Race.Undead, PlayerClassType.Assassin);
-
-Console.WriteLine($"ID:{thirdPlayer.Id}, Character Name:{thirdPlayer.Name}, Character Level:{thirdPlayer.Level}, Character Favourite Number:{thirdPlayer.FavouriteNumber}");
-
-
+}
+accounts.ForEach(x => Console.WriteLine($"User Name:{x.UserName},Password:{x.Password},Platform:{x.Platform},Level:{x.Level},Game Name:{x.GameName},Price:{x.Price}"));
 Console.ReadLine();
+
+
+
+//using GFA.OOP.Enums;
+//using GFA.OOP.Models;
+
+//var firstPlayer = new Player("Gandhalf 38", 38, Race.Human,PlayerClassType.Mage);
+
+//Console.WriteLine($"ID:{firstPlayer.Id}, Character Name:{firstPlayer.Name}, Character Level:{firstPlayer.Level}, Character Favourite Number:{firstPlayer.FavouriteNumber}");
+
+//firstPlayer.LevelUp();
+//firstPlayer.LevelUp();
+//firstPlayer.LevelUp();
+//firstPlayer.LevelUp();
+//firstPlayer.LevelUp();
+
+//Console.WriteLine($"ID:{firstPlayer.Id}, Character Name:{firstPlayer.Name}, Character Level:{firstPlayer.Level}, Character Favourite Number:{firstPlayer.FavouriteNumber}");
+
+//firstPlayer.ChangeRace(Race.Dwarf);
+
+//Console.WriteLine(firstPlayer.Race.ToString());
+
+//var secondPlayer = new Player("Excalibur",10,Race.Orc,PlayerClassType.DeathKnight);
+
+//Console.WriteLine($"ID:{secondPlayer.Id}, Character Name:{secondPlayer.Name}, Character Level:{secondPlayer.Level}, Character Favourite Number:{secondPlayer.FavouriteNumber}");
+
+//var thirdPlayer = new Player("Tarkan", 55, Race.Undead, PlayerClassType.Assassin);
+
+//Console.WriteLine($"ID:{thirdPlayer.Id}, Character Name:{thirdPlayer.Name}, Character Level:{thirdPlayer.Level}, Character Favourite Number:{thirdPlayer.FavouriteNumber}");
+
+
+//Console.ReadLine();
 
 
